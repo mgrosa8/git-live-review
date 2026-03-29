8ca5a429bcd353cced2109c8b7883ac5b350c5c6
mariarosa <mgrosaledesma@gmail.com>
2026-03-29 12:34:28 -0400

    call multiply() in main()

---
+++ src/day01_commit_push_pull/Main.java
@@ -0,0 +1,8 @@
+package day01_commit_push_pull;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+        System.out.println("First change: practicing Git commit");
+    }
+}
