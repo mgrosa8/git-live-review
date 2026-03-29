3e2007e76568a00363873c2730f1b0aed4f8da57
mgrosa8 <mariadr809@hotmail.com>
2026-03-29 12:15:31 -0400

    add multiply function to SimpleCalculator

--- src/day02_pull_clone_rollback/SimpleCalculator.java
+++ src/day02_pull_clone_rollback/SimpleCalculator.java
@@ -10,11 +10,15 @@ public class SimpleCalculator {
         public static int subtract(int a, int b) {
             return a - b;
         }
+        public static int multiply(int a, int b) {
+            return a * b;
+        }
 
         public static void main(String[] args) {
             int x = 10, y = 5;
             System.out.println("Add: " + add(x, y));
             System.out.println("Subtract: " + subtract(x, y));
         }
+        
 
 }
